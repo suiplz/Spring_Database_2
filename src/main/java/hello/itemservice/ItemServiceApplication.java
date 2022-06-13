71d188e579cf1d967f0f9ba715af3459a732f691
suiplz <acepade231@gmail.com>
2022-06-13 18:10:22 +0900

    [JPA, SpringDataJPA, QueryDSL] 22-06-13

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -13,7 +13,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @Slf4j
-@Import(MyBatisConfig.class)
+@Import(V2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
@@ -26,17 +26,5 @@ public class ItemServiceApplication {
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
 	}
-//
-//	@Bean
-//	@Profile("test")
-//	public DataSource dataSource() {
-//		log.info("메모리 데이터베이스 초기화");
-//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		dataSource.setDriverClassName("org.h2.Driver");
-//		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-//		dataSource.setUsername("sa");
-//		dataSource.setPassword("");
-//		return dataSource;
-//
-//	}
+
 }
